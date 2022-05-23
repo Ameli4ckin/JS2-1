1047570d0dcd0dc23eef9b3783dcd2752ba56b8c
iZmailvii <iameli4ckin@mail.ru>
2022-05-23 12:15:23 +0300

    add cart, cards, img

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const goods = [
-    { title: 'Shirt', price: 150 }, 
-    { title: 'Socks', price: 50 }, 
-    { title: 'Jacket', price: 350 }, 
-    { title: 'Shoes', price: 250 },
+    {title: 'Shirt', foto: 'example.jpeg', price: 150}, 
+    {title: 'Socks', foto: 'example.jpeg', price: 50}, 
+    {title: 'Jacket', foto: 'example.jpeg', price: 350}, 
+    {title: 'Shoes', foto: 'example.jpeg', price: 250},
 ];
     
-const renderGoodsItem = (title, price) => {
+const renderGoodsItem = (title, foto, price,...anyInfo) => {
     return `<div class="goods-item">
                 <h3>${title}</h3>
-                <p>${price}</p>
+                <img class = "goods-img" src="${foto}"></img> 
+                <p>${price} $</p>
+                <button class="buy-btn">Купить</button>
             </div>`;
 };
     
-const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+const renderGoodsList = list => {
+    let goodsList = list.map(item => renderGoodsItem(item.title,item.foto,item.price)).join('');
     document.querySelector('.goods-list').innerHTML = goodsList;
+
 } 
+// const renderGoodsAnyInfo = listAny => {
+//     let goodsAnyInfo = list.map(item => renderGoodsItem(item.anyInfo)).join('');
+//     document.querySelector('goods-item').innerHTML = goodsAnyInfo;
+// }
+//При присваивании массива свойству .innerHTML, у него автоматически вызывается метод .toString, который эквивалентен вызову метода .join(',')
+
 
 renderGoodsList(goods);
